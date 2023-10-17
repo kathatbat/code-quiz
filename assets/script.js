e257615994e1efd66b41c2abca6c418c42a6f8c5
kathatbat <katoria1234@icloud.com>
2023-10-16 21:58:08 -0500

    updates js

--- assets/script.js
+++ assets/script.js
@@ -1,93 +1,96 @@
-var  startPage = document.querySelector("start-page");
-var  startButton = document.querySelector("start-button");
+var  startPage = document.querySelector("#start-page");
+var  startButton = document.querySelector("#start-button");
 
-var questionPage = document.querySelector("question-page");
-var questionDisplay = document.querySelector("question");
-var choicesDisplay = document.querySelector("choices");
-var feedbackDisplay =  document.querySelector("feedback");
+var questionPage = document.querySelector("#question-page");
+var questionDisplay = document.querySelector("#question");
+var choicesDisplay = document.querySelector("#choices");
+var feedbackDisplay =  document.querySelector("#feedback");
 
 var currentQuestion = 0;
+var timer;
+var currentTime = 40;
+var timeElement = document.querySelector('#time');
 
 var quizQuestions = [
     {
-        q : "What HTML stand for?", 
+        q : "What does HTML stand for?", 
         a : ["Hyperlink and Text Markup Language", "Hypertext Markup Language", "Hyper Transfer Markup Language","High-Level Markup Language"],
         correctA : "Hypertext Markup Language"
     }, 
 
     {
-        q : "", 
-        a : ["", "", "",""],
-        correctA : ""
+        q : "What does CSS stand for?", 
+        a : ["Color Scheme Starter", "Central Styling Sheet", "Cascading Style Sheet","Cascading Shield Sorts"],
+        correctA : "Cascading Style Sheet"
     }, 
 
     {
-        q : "", 
-        a : ["", "", "",""],
-        correctA : ""
+        q : "Which ancient civilization worshipped cats?", 
+        a : ["Egyptians", "China", "Mesopotamia","They worshipped dogs."],
+        correctA : "Egyptians"
     }, 
 
     {
-        q : "", 
-        a : ["", "", "",""],
-        correctA : ""
+        q : "What percentage of their lives do cats sleep?", 
+        a : ["50%", "20%", "80%","70%"],
+        correctA : "70%"
     }, 
 
     {
-        q : "", 
-        a : ["", "", "",""],
-        correctA : ""
+        q : "Cats walk like?", 
+        a : ["Elephants", "Camels and Giraffes", "Gazzeles","Penguins"],
+        correctA : "Camels and Giraffes"
     }, 
 
     {
-        q : "", 
-        a : ["", "", "",""],
-        correctA : ""
+        q : "Who invented the cat door?", 
+        a : ["Issac Newton", "Benjamin Franklin", "Einstein","Ur mom"],
+        correctA : "Issac Newton"
     }, 
 
     {
-        q : "", 
-        a : ["", "", "",""],
-        correctA : ""
+        q : "A house cat can reach up to what speed?", 
+        a : ["50 mph", "20 mph", "40 mph","30 mph"],
+        correctA : "30 mph"
     }, 
 ];
 
 questionPage.style.display = 'none';
 
 function displayQuestions(){
-    questionDisplay.textContent = questions[currentQuestion].question;
+    questionDisplay.textContent = quizQuestions[currentQuestion].q;
     choicesDisplay.innerHTML = "";
 
-    for (i = 0; i < questions[currentQuestion].choices.length; i++){
+    for (i = 0; i < quizQuestions[currentQuestion].a.length; i++){
         var listItem = document. createElement("li");
         choicesDisplay.appendChild(listItem);
-        listItem.textContent = questions[currentQuestion].choices[i];
+        listItem.textContent = quizQuestions[currentQuestion].a[i];
     }
 }
 
 startButton.addEventListener('click', function () {
     currentQuestion = 0;
     currentTime = 75;
-    loadQuestion();
+    displayQuestions();
     startPage.style.display = 'none';
-    viewHighscore.style.display = 'none';
+    // viewHighscore.style.display = 'none';
     timeElement.style.display = 'block';
     questionPage.style.display = 'block';
 
-    timeDisplay.textContent = currentTime;
+    timeElement.textContent = currentTime;
 
     timer = setInterval(function () {
         currentTime--;
        
         if(currentTime <= 0) {
             questionPage.style.display = 'none';
-            endPage.style.display = 'block';
+            // endPage.style.display = 'block';
             clearInterval(timer);
-            currentTime = 0;
-            scoreDisplay.textContent = currentTime;
+            // currentTime = 0;
+            // scoreDisplay.textContent = currentTime;
         }
        
-        timeDisplay.textContent = currentTime;
+        timeElement.textContent = currentTime;
     }, 1000)
 });
 
@@ -98,7 +101,8 @@ choicesDisplay.addEventListener('click', function (event) {
     }
    
     var userChoice = event.target;
-    if (userChoice.textContent === questions[currentQuestion].correctAnswer) {
+    console.log(userChoice.textContent, quizQuestions[currentQuestion].correctA);
+    if (userChoice.textContent === quizQuestions[currentQuestion].correctA) {
         feedbackDisplay.textContent = "Correct!";
         feedbackDisplay.style.color = "#99ffc7";
     }
@@ -112,7 +116,7 @@ choicesDisplay.addEventListener('click', function (event) {
         feedbackDisplay.textContent = "";
         currentQuestion++;
 
-        if (currentQuestion == questions.length) {
+        if (currentQuestion == quizQuestions.length) {
             timeElement.style.display = 'none';
             questionPage.style.display = 'none';
             endPage.style.display = 'block';
@@ -120,11 +124,11 @@ choicesDisplay.addEventListener('click', function (event) {
             scoreDisplay.textContent = currentTime;
         }
         else {
-            loadQuestion();
+            displayQuestions();
         }
     }, 1000)
 });
 
-mainButton.addEventListener('click', function(){
-    startPage.style.display = "block";
-})
+// mainButton.addEventListener('click', function(){
+//     startPage.style.display = "block";
+// })
